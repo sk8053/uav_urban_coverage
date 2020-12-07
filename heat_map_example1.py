f2b829de976f3e8430249c5f6f5bd40de9097606
sk8053 <sk8053@nyu.edu>
2020-12-06 22:08:21 -0500

    example

--- heat_map_example1.py
+++ heat_map_example1.py
@@ -13,9 +13,10 @@ f = Heat_Map(mod_name='uav_beijing', bs_type = 'Aerial',
       #          annot= True, plane_type='xy', nsect=3, cdf_prob=0.3)
 #f.plot_heat_map(bs_type="Aerial", tilt_angle= 45, get_link_state= True, annot= False, nsect =3, plane_type= 'xy')
 f.plane_type = 'xy'
-f.net_work_area_hori=np.linspace(-100, 100, 10)
-f.net_work_area_verti = np.linspace(-100, 100, 10)
-f.cdf_prob = 0.5 # take meadian value
-f.get_association(aerial_height=10, tilt_angel_t=-22, tilt_angle_a=45, plane_shift=30)
+f.net_work_area_hori=np.linspace(-100, 800, 11)
+f.net_work_area_verti = np.linspace(-100, 800, 11)
+f.cdf_prob = 0.1 # take meadian value
+#f.get_association(aerial_height=10, tilt_angel_t=-22, tilt_angle_a=45, plane_shift=30)
+f.plot_heat_map(bs_type="Terrestrial", tilt_angle= -22, get_link_state= True, annot= False, nsect =1, plane_type= 'xy')
 #f.get_association(aerial_height=10, tilt_angel_t=-22, tilt_angle_a=45)
 plt.show()
