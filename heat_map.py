1694e3eb7284d0a162dc4aad3fc396755ebf7113
sk8053 <sk8053@nyu.edu>
2020-12-07 19:03:10 -0500

    heat_map modified so that it can set the height of  aerial BSs 3

--- heat_map.py
+++ heat_map.py
@@ -112,8 +112,7 @@ class Heat_Map(object):
 
         cmap = plt.get_cmap(self.cmap_name)
         x = self.net_work_area_hori
-        if (bs_type == 'Aerial' and plane_type == 'xz') or \
-                (bs_type == 'Aerial' and plane_type == 'yz'):
+        if plane_type == 'xz' or plane_type == 'yz':
             y = self.net_work_area_verti - aerial_height
         else:
             y = self.net_work_area_verti
