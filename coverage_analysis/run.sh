74eb102471895e0ff821d3a82091b75fe9980bca
sk8053 <sk8053@nyu.edu>
2021-04-06 14:36:47 -0400

    remove some files

---
+++ coverage_analysis/run.sh
@@ -0,0 +1,5 @@
+python3 example1.py --height 30 --max_iter 100 --n_uav 100
+python3 example1.py --height 120 --max_iter 100 --n_uav 100
+python3 example1.py --height 60 --max_iter 100 --n_uav 100
+
+
--- run.sh
+++
@@ -1,5 +0,0 @@
-python3 example1.py --height 30 --max_iter 1000 --n_uav 1000
-python3 example1.py --height 120 --max_iter 1000 --n_uav 1000
-python3 example1.py --height 60 --max_iter 1000 --n_uav 1000
-
-
